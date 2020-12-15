f542126df9b78f969abe879530010e613079c996
zappacom1027 <zappa.com@qq.com>
2020-12-15 15:53:16 +0800

    添加围栏代码

---
+++ pre-system/src/main/java/com/xd/pre/modules/fence/mapper/IotFenceMapper.java
@@ -0,0 +1,16 @@
+package com.xd.pre.modules.fence.mapper;
+
+import com.xd.pre.modules.fence.domain.IotFence;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 围栏信息表 Mapper 接口
+ * </p>
+ *
+ * @author phlink
+ * @since 2020-12-09
+ */
+public interface IotFenceMapper extends BaseMapper<IotFence> {
+
+}
